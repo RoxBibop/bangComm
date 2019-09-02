0592125698a6cb8cfaa34813322f3ee53ef88fbc
RoxBibop <thibault.malik@gmail.com>
2019-09-02 11:45:26 +0200

    css déimportisé

--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -8,6 +8,13 @@
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/app.min.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/emploi.min.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/footer.min.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/pouvoirs.min.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/var.min.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/nav.min.css">
+
+
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
