a0e09eb22c2f0422bbfca9df8b2cdeb19601d22f
RoxBibop <thibault.malik@gmail.com>
2019-09-02 16:49:02 +0200

    fin texte page emploi(need paralax now)

--- wp-content/themes/html5blank-stable/emploi.php
+++ wp-content/themes/html5blank-stable/emploi.php
@@ -24,10 +24,69 @@
     <div class="e_button">
         <a class="e_savoirplus" href="#">en savoir plus</a>
     </div>
-
 </section>
 
 <section class="e_creons">
-
+    <div class="e_titre2">
+        <h2>créons la différence</h2>
+        <p>Nous mettons notre expertise et notre<br>
+           créativité à votre service pour vous<br>
+           permettre de vous démarquer et de vous</p>
+    </div>
+    <div class="e_hashtags2">
+        <div class="e_msg">
+            <h5>concevoir le message</h5>
+            <p>#pitch commercial #création de contenus<br>#identité visuelle</p>
+        </div>
+        <div class="e_deter">
+            <h5>déterminer le plan de communication</h5>
+            <p>quels support ? quels médias ?<br>quand ? quels coûts ?</p>
+        </div>
+        <div class="e_cre">
+            <h5>créer vos supports</h5>
+            <p>imprimés, press, digital, réseaux sociaux;<br>vidéos, salons, goodies...</p>
+        </div>
+        <div class="e_sel">
+            <h5>sélectionner et/ou piloter les prestataires</h5>
+            <p>digital, imprimer, enseigne, photographie...</p>
+        </div>
+    </div>
+</section>
+<section class="e_cultiver">
+    <hr class="hr1">
+    <div class="e_divpasplus">
+        <p class="e_pasplus">ne m'en dites pas plus...</p>
+        <div class="e_fonrouge">
+            <a class="e_costume"href="#">j'enfile mon collant</a>
+        </div>
+    </div>
+    <hr class="hr2">
+    <div class="e_titre3">
+        <h2>cultivons vos talents</h2>
+        <p>Nous proposons les outils qui<br>
+           professionnalisent votre communication<br>
+           et mettent en lumoère les talents de votre<br>entreprise.</p>
+    </div>
+    <div class="e_hashtags">
+        <div>
+            <h5>former vos talents</h5>
+            <p>logiciels d'infographie, mise à jour de site web,<br>réseaux sociaux</p>
+        </div>
+        <div>
+            <h5>vous donner de l'autonomie</h5>
+            <p>création et utilisation de gabarits<br>outils pratiques</p>
+        </div>
+        <div>
+            <h5>piloter votre communication</h5>
+            <p>tableau de bord, pilotage des prestataires</p>
+        </div>
+        <div>
+            <h5>animer votre communication</h5>
+            <p>animation de brainstoming<br>comité de rédaction</p>
+        </div>
+    </div>
+    <div class="e_button">
+        <a class="e_savoirplus" href="#">en savoir plus</a>
+    </div>
 </section>
 
