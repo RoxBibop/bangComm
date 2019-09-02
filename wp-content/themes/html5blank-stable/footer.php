66de6ce3f66ed89170f6eeaffb3cf63a71b1d537
ClemMarkey <markey.clement@gmail.com>
2019-09-02 16:49:21 +0200

    fin texte page pouvoirs (need parallax now) + Footer quasi terminé

--- wp-content/themes/html5blank-stable/footer.php
+++ wp-content/themes/html5blank-stable/footer.php
@@ -1,30 +1,85 @@
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
+				<div class="desc">
+					<p>
+						Au service de la communication (et des super-héros) depuis 2009, nous sommes implantés sur
+						la Côte d’Opale, mais nous volons à votre secours sur toute la
+						France
+					</p>
+
+					<p>
+						<span class="big"><?php bloginfo('name'); ?></span>
+						<br>
+						31 bd des Alliés
+						<br>
+						62100 Calais, FRANCE
+						<br>
+					</p>
+
+					<p class="big">03 21 35 56 91</p>
+
+					<p>
+						Sur RDV du lundi au vendredi : <br>
+						9h à 12h - 14h à 17h
+					</p>
+
+				</div>
+
+				<nav class="nav">
+					<ul>
+						<div class="line line-1"></div>
+						<li class ="li-1"><a href="#">Nos pouvoirs</a></li>
+
+						<div class="line line-2"></div>
+						<li class ="li-2"><a href="#">Le mode d'emploi</a></li>
+
+						<div class="line line-3"></div>
+						<li class ="li-3"><a href="#">L'agence</a></li>
+
+						<div class="line line-4"></div>
+						<li class ="li-4"><a href="#">Actus</a></li>
+
+						<div class="line line-5"></div>
+						<li class ="li-5"><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+
+				<form action="" class="contact">
+					<h3>Contactez-nous !</h3>
+					<span>(On ne mord pas, enfin pas encore...)</span>
+					<input type="text" placeholder="Prénom / Nom">
+					<input type="email" name="email" id="email" placeholder="Adresse mail">
+					<textarea name="message" id="message" cols="30" rows="10"></textarea>
+					<button type="submit">Envoyer</button>
+				</form>
+
 
 			</footer>
 			<!-- /footer -->
 
-		</div>
-		<!-- /wrapper -->
+			</div>
+			<!-- /wrapper -->
 
-		<?php wp_footer(); ?>
+			<?php wp_footer(); ?>
 
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
+			<!-- analytics -->
+			<script>
+				(function(f, i, r, e, s, h, l) {
+					i['GoogleAnalyticsObject'] = s;
+					f[s] = f[s] || function() {
+						(f[s].q = f[s].q || []).push(arguments)
+					}, f[s].l = 1 * new Date();
+					h = i.createElement(r),
+						l = i.getElementsByTagName(r)[0];
+					h.async = 1;
+					h.src = e;
+					l.parentNode.insertBefore(h, l)
+				})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+				ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+				ga('send', 'pageview');
+			</script>
 
-	</body>
-</html>
+			</body>
+
+			</html>
--- wp-content/themes/html5blank-stable/js/scripts.js
+++ wp-content/themes/html5blank-stable/js/scripts.js
@@ -5,7 +5,75 @@
 		'use strict';
 		
 		// DOM ready, take it away
+
+
+		// Apparition de la ligne supérieure
+		// Nav du footer
+
+		// Ligne 1
+
+		var li1 = document.querySelector('.li-1');
+
+		li1.addEventListener('mouseover', activel1);
+		li1.addEventListener('mouseout', offl1);
+
+		function activel1(){
+			let ligne = document.querySelector('.line');
+			ligne.classList.add('full-line-1');
+		}
+
+		function offl1(){
+			let ligne = document.querySelector('.line');
+			ligne.classList.remove('full-line-1');
 		
+		}
+
+		// Ligne 2
+
+		var li2 = document.querySelector('.li-2');
+
+		li2.addEventListener('mouseover', activel2);
+		li2.addEventListener('mouseout', offl2);
+
+		function activel2(){
+			let ligne = document.querySelector('.line-2');
+			ligne.classList.add('full-line-2');
+		}
+
+		function offl2(){
+			let ligne = document.querySelector('.line-2');
+			ligne.classList.remove('full-line-2');
+		
+		}
+
+		// Ligne 3
+
+		var li3 = document.querySelector('.li-3');
+
+		li3.addEventListener('mouseover', activel3);
+		li3.addEventListener('mouseout', offl3);
+
+		function activel3(){
+			let ligne = document.querySelector('.line-3');
+			ligne.classList.add('full-line-3');
+		}
+
+		function offl3(){
+			let ligne = document.querySelector('.line-3');
+			ligne.classList.remove('full-line-3');
+		
+		}
+
+		// Ligne 4
+
+
+
+		// Ligne 5
+
+
+
+
+
 	});
 	
 })(jQuery, this);
