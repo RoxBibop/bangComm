5c11392bfea7e97003377fcf7c390fc9e35318a6
ClemMarkey <markey.clement@gmail.com>
2019-09-02 14:09:51 +0200

    inté pouvoirs V1

--- wp-content/themes/html5blank-stable/pouvoirs.php
+++ wp-content/themes/html5blank-stable/pouvoirs.php
@@ -3,52 +3,62 @@
 
 <section class="p_section">
 
+    <hr>
+
     <h1>Nos pouvoirs</h1>
     <span>/// Lancer une activité avec un budget de créateur</span>
 
-    <div class="p_bloc">
-        <h2>On s'appelle ? On boit un café ?</h2>
-        <p>
-            Une réunion de travail est indispensable pour mettre les
-            choses au clair. Elle permet de déterminer les <strong>objectifs</strong> de
-            votre communication et définir les <strong>priorités</strong> par cible.
-        </p>
+    <div class="tronc">
+        <div class="p_flex end">
+            <div class="p_bloc">
+                <h2>On s'appelle ? On boit un café ?</h2>
+                <p>
+                    Une réunion de travail est indispensable pour mettre les
+                    choses au clair. Elle permet de déterminer les <strong>objectifs</strong> de
+                    votre communication et définir les <strong>priorités</strong> par cible.
+                </p>
+            </div>
+        </div>
+        <div class="p_flex start">
+            <div class="p_bloc">
+                <h2>Dans le mille</h2>
+                <p>
+                    Grâce à ces précieuses informations, nous ciblons avec
+                    vous des supports pertinents et adaptés à votre
+                    situation. Nous faisons notre maximum pour que ces
+                    outils soient <strong>réutilisables</strong> et simple à modifier par vos
+                    soins.
+                </p>
+            </div>
+        </div>
+
+        <div class="p_flex start">
+            <div class="p_bloc">
+                <h2>On vous forme même à ça</h2>
+                <p>
+                    Dans le but de limiter les coûts sur votre budget de
+                    créateur, <strong>nous vous formons</strong> à l’utilisation des outils.
+                    Nous créons ces derniers pour qu’ils soient les plus
+                </p>
+            </div>
+        </div>
+
+        <div class="p_flex end">
+            <div class="p_bloc">
+                <h2>À nous de jouer !</h2>
+                <p>
+                    Nous nous retroussons les manches et nous
+                    mettons notre créativité et nos idées au service de
+                    votre communication pour créer des <strong>outils
+                        efficaces et visuellement attractifs.</strong>
+                </p>
+            </div>
+        </div>
+
+        <blockquote>
+            Tout le monde a du talent. <br> Ce qui est plus rare, c'est d'avoir <br> le courage de développer ce talent. <br>
+            <cite>Erica JONG</cite>
+        </blockquote>
     </div>
-
-    <div class="p_bloc">
-        <h2>Dans le mille</h2>
-        <p>
-            Grâce à ces précieuses informations, nous ciblons avec
-            vous des supports pertinents et adaptés à votre
-            situation. Nous faisons notre maximum pour que ces
-            outils soient <strong>réutilisables</strong> et simple à modifier par vos
-            soins.
-        </p>
-    </div>
-
-    <div class="p_bloc">
-        <h2>On vous forme même à ça</h2>
-        <p>
-            Dans le but de limiter les coûts sur votre budget de
-            créateur, <strong>nous vous formons</strong> à l’utilisation des outils.
-            Nous créons ces derniers pour qu’ils soient les plus
-        </p>
-    </div>
-
-    <div class="p_bloc">
-        <h2>À nous de jouer</h2>
-        <p>
-            Nous nous retroussons les manches et nous
-            mettons notre créativité et nos idées au service de
-            votre communication pour créer des <strong>outils
-                efficaces et visuellement attractifs.</strong>
-        </p>
-    </div>
-
-
-    <blockquote>
-        Tout le monde a du talent. Ce qui est plus rare, c'est d'avoir le courage de développer ce talent.
-        <cite>Erica JONG</cite>
-    </blockquote>
 
 </section>
