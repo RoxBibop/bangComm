2afd61f8ebc88d8688539961544c447e6cb57ee7
RoxBibop <thibault.malik@gmail.com>
2019-09-02 14:35:11 +0200

    app réparé(import qui faisait du caca)

--- wp-content/themes/html5blank-stable/emploi.php
+++ wp-content/themes/html5blank-stable/emploi.php
@@ -1,9 +1,33 @@
 <?php /* Template Name: Emploi */ get_header(); ?>
+
 <section class="e_sec">
+    
     <div class="e_titre">
         <h1>Le mode d'emploi</h1>
         <p>Comment devenir un super-communicant ?</p>
     </div>
+
+    <div class="e_valeur">
+        <h2>révélons vos valeurs</h2>
+        <p>Ensemble, nous révélons le meilleur de vos<br>entreprises et de vos savoir-faire.</p>
+    </div>
+    <div class="e_hashtags">
+        <div class="e_analyser">
+            <h5>analyser votre activité</h5>
+            <p>#audit #enquête #concurrence</p>
+        </div>
+        <div class="e_determiner">
+            <h5>déterminer votre stratégie<br>de communication</h5>
+            <p>animation d'ateliers com'<br>brainstorming</p>
+        </div>
+    </div>
+    <div class="e_button">
+        <a class="e_savoirplus" href="#">en savoir plus</a>
+    </div>
+
 </section>
 
+<section class="e_creons">
+
+</section>
 
