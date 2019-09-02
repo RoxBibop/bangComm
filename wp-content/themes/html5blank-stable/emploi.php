2a1904fed53a04e1434aebe6c1e5ab0cb0d9a990
RoxBibop <thibault.malik@gmail.com>
2019-09-02 11:38:40 +0200

    polices

--- wp-content/themes/html5blank-stable/emploi.php
+++ wp-content/themes/html5blank-stable/emploi.php
@@ -1,2 +1,9 @@
-<?php /* Template Name: Emploi */ get_header(); get_template_part("nav"); ?>
+<?php /* Template Name: Emploi */ get_header(); ?>
+<section class="e_sec">
+    <div class="e_titre">
+        <h1>Le mode d'emploi</h1>
+        <p>Comment devenir un super-communicant ?</p>
+    </div>
+</section>
+
 
