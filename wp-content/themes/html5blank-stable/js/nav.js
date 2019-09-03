d6b32dfa8ac788bcef3b70f88dbf94a8d00f7d40
RoxBibop <thibault.malik@gmail.com>
2019-09-03 13:23:22 +0200

    navbar finie ou presque

--- wp-content/themes/html5blank-stable/js/nav.js
+++ wp-content/themes/html5blank-stable/js/nav.js
@@ -1,6 +1,19 @@
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/app.min.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/emploi.min.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/footer.min.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/pouvoirs.min.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/var.min.css">
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/nav.min.css"></link>
+
+		var btn = document.querySelector('.toggle');
+		var hdr = document.querySelector(".header");
+		var nav = document.querySelector('.navbar');
+		
+		function tgle()
+		{
+			btn.classList.toggle("togactive");
+			hdr.classList.toggle("headerActive");
+	
+			setTimeout(function()
+			{
+				nav.classList.toggle("navactive")
+			}, 500);
+		}
+	
+		btn.addEventListener('click', tgle);
+		console.log('niktamer');
+
--- wp-content/themes/html5blank-stable/js/scripts.js
+++ wp-content/themes/html5blank-stable/js/scripts.js
@@ -28,66 +28,8 @@
 		
 		}
 
-		// Ligne 2
-
-		var li2 = document.querySelector('.li-2');
-
-		li2.addEventListener('mouseover', activel2);
-		li2.addEventListener('mouseout', offl2);
-
-		function activel2(){
-			let ligne = document.querySelector('.line-2');
-			ligne.classList.add('full-line-2');
-		}
-
-		function offl2(){
-			let ligne = document.querySelector('.line-2');
-			ligne.classList.remove('full-line-2');
-		
-		}
-
-		// Ligne 3
-
-		var li3 = document.querySelector('.li-3');
-
-		li3.addEventListener('mouseover', activel3);
-		li3.addEventListener('mouseout', offl3);
-
-		function activel3(){
-			let ligne = document.querySelector('.line-3');
-			ligne.classList.add('full-line-3');
-		}
-
-		function offl3(){
-			let ligne = document.querySelector('.line-3');
-			ligne.classList.remove('full-line-3');
-		
-		}
-
-		// Ligne 4
-
-		var li4 = document.querySelector('.li-4');
-
-		li4.addEventListener('mouseover', activel4);
-		li4.addEventListener('mouseout', offl4);
-
-		function activel4(){
-			let ligne = document.querySelector('.line-4');
-			ligne.classList.add('full-line-4');
-		}
-
-		function offl4(){
-			let ligne = document.querySelector('.line-4');
-			ligne.classList.remove('full-line-4');
-		
-		}
-
-		// Ligne 5
-
-
-
-
-
 	});
 	
 })(jQuery, this);
+
+
