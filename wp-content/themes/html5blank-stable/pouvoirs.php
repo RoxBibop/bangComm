6f56765de53c2c3a5e288aa555b5f41255c93e5c
ClemMarkey <markey.clement@gmail.com>
2019-09-03 12:08:25 +0200

    nouvelle inté pouvoirs :)

--- wp-content/themes/html5blank-stable/pouvoirs.php
+++ wp-content/themes/html5blank-stable/pouvoirs.php
@@ -3,62 +3,59 @@
 
 <section class="p_section">
 
-    <hr>
+    <!-- <hr> -->
 
-    <h1>Nos pouvoirs</h1>
-    <span>/// Lancer une activité avec un budget de créateur</span>
+    <h2>Étude de cas</h2>
+    <p class="subtitle">Ils sont devenus « super » communicants...</p>
 
     <div class="tronc">
         <div class="p_flex end">
             <div class="p_bloc">
-                <h2>On s'appelle ? On boit un café ?</h2>
-                <p>
-                    Une réunion de travail est indispensable pour mettre les
-                    choses au clair. Elle permet de déterminer les <strong>objectifs</strong> de
-                    votre communication et définir les <strong>priorités</strong> par cible.
+                <p class="p_titre">
+                    Révéler les arguments concurrentiels de l'entripre
                 </p>
-            </div>
-        </div>
-        <div class="p_flex start">
-            <div class="p_bloc">
-                <h2>Dans le mille</h2>
-                <p>
-                    Grâce à ces précieuses informations, nous ciblons avec
-                    vous des supports pertinents et adaptés à votre
-                    situation. Nous faisons notre maximum pour que ces
-                    outils soient <strong>réutilisables</strong> et simple à modifier par vos
-                    soins.
+                <p class="p_tag">
+                    / Brainstorming / Concept / Contnus / Supports de Communication /
                 </p>
             </div>
         </div>
 
         <div class="p_flex start">
             <div class="p_bloc">
-                <h2>On vous forme même à ça</h2>
-                <p>
-                    Dans le but de limiter les coûts sur votre budget de
-                    créateur, <strong>nous vous formons</strong> à l’utilisation des outils.
-                    Nous créons ces derniers pour qu’ils soient les plus
+                <p class="p_titre">
+                    Piloter un projet web et créer les contenus
+                </p>
+                <p class="p_tag">
+                    / Accompagnement / Site Web / Pilotage /
+                    Redaction /
                 </p>
             </div>
         </div>
 
         <div class="p_flex end">
             <div class="p_bloc">
-                <h2>À nous de jouer !</h2>
-                <p>
-                    Nous nous retroussons les manches et nous
-                    mettons notre créativité et nos idées au service de
-                    votre communication pour créer des <strong>outils
-                        efficaces et visuellement attractifs.</strong>
+                <p class="p_titre">
+                    Le plus bel outil de communication, c’est vous !
+                </p>
+                <p class="p_tag">
+                    / Formation / Template / Tableau de Bord /
                 </p>
             </div>
         </div>
 
-        <blockquote>
-            Tout le monde a du talent. <br> Ce qui est plus rare, c'est d'avoir <br> le courage de développer ce talent. <br>
-            <cite>Erica JONG</cite>
-        </blockquote>
+        <div class="p_flex start">
+            <div class="p_bloc">
+                <p class="p_titre">
+                    Plus qu’un logo, une vraie identité…
+                </p>
+                <p class="p_tag">
+                    / Logo / Charte Graphique / Moodboard /
+                </p>
+            </div>
+        </div>
+
+    <div class="bandeau">
+
     </div>
 
 </section>
